fe3d13485ea9a6cef122040b76112f2123be6da6
Roket_Flame <danil.slinchuk@gmail.com>
2023-03-24 23:01:56 +0300

    feat: change generics in Tree, now it has more clear structure

--- src/main/kotlin/ABSTree.kt
+++ src/main/kotlin/ABSTree.kt
@@ -4,28 +4,27 @@ interface Tree<T : Comparable<T>> {
     fun delete(data: T)
 }
 
-abstract class ABSTree<K : Comparable<K>, V : Any> : Tree<KeyValue<K, V>> {
-    var root: BSNode<K, V>? = null
-    fun simple_add(data: KeyValue<K, V>) {
-        val node = BSNode(data)
+abstract class ABSTree<T : Comparable<T>, NodeType : Node<T, NodeType>> : Tree<T> {
+    var root: NodeType? = null
+    fun simple_add(data: NodeType) {
         if (root == null) {
-            root = node
+            root = data
             return
         }
         var curNode = root!!
         while (true) {
 
-            if (node < curNode) {
+            if (data < curNode) {
                 if (curNode.left == null) {
-                    curNode.left = node
+                    curNode.left = data
                     break
                 } else {
                     curNode = curNode.left!!
                 }
 
-            } else if (node > curNode) {
+            } else if (data > curNode) {
                 if (curNode.right == null) {
-                    curNode.right = node
+                    curNode.right = data
                     break
                 } else {
                     curNode = curNode.right!!
--- src/main/kotlin/Node.kt
+++ src/main/kotlin/Node.kt
@@ -1,10 +1,14 @@
-interface Node<T : Comparable<T>, Subtype : Node<T, Subtype>> {
+interface Node<T : Comparable<T>, Subtype : Node<T, Subtype>> : Comparable<Node<T, Subtype>> {
+    override operator fun compareTo(other: Node<T, Subtype>): Int {
+        return data.compareTo(other.data)
+    }
+
     var data: T
     var left: Subtype?
     var right: Subtype?
 }
 
-class BSNode<K : Comparable<K>, V : Any>(override var data: KeyValue<K, V>) :  Node<KeyValue<K, V>, BSNode<K, V>> {
+class BSNode<K : Comparable<K>, V : Any>(override var data: KeyValue<K, V>) : Node<KeyValue<K, V>, BSNode<K, V>> {
     override var left: BSNode<K, V>? = null
     override var right: BSNode<K, V>? = null
 
@@ -15,6 +19,7 @@ class BSNode<K : Comparable<K>, V : Any>(override var data: KeyValue<K, V>) :  N
     fun getValue(): V {
         return data.getValue()
     }
+
     operator fun compareTo(other: BSNode<K, V>?): Int {
         if (other != null) {
             return data.getKey().compareTo(other.getKey())
@@ -27,9 +32,11 @@ class KeyValue<K : Comparable<K>, V>(private val key: K, private var value: V) :
     fun getKey(): K {
         return key
     }
+
     fun getValue(): V {
         return value
     }
+
     override fun compareTo(other: KeyValue<K, V>): Int {
         return key.compareTo(other.key)
     }
