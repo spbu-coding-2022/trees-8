f7a83f8921dfc31ccb01e1578b1961745090506b
Roket_Flame <danil.slinchuk@gmail.com>
2023-03-24 22:33:16 +0300

    fix: fix generics in interface Tree feat: release simple_add function

--- src/main/kotlin/ABSTree.kt
+++ src/main/kotlin/ABSTree.kt
@@ -1,16 +1,37 @@
-import Node
-interface Tree {
-    fun add()
-    fun contain()
-    fun delete()
+interface Tree<T : Comparable<T>> {
+    fun add(data: T)
+    fun contain(data: T)
+    fun delete(data: T)
 }
 
-abstract class ABSTree<K : Comparable<K>, V : Any>: Tree {
+abstract class ABSTree<K : Comparable<K>, V : Any> : Tree<KeyValue<K, V>> {
     var root: BSNode<K, V>? = null
-    fun simple_add(key: K, value: V) {
-        val node = BSNode(KeyValue(key, value))
+    fun simple_add(data: KeyValue<K, V>) {
+        val node = BSNode(data)
+        if (root == null) {
+            root = node
+            return
+        }
+        var curNode = root!!
         while (true) {
+
+            if (node < curNode) {
+                if (curNode.left == null) {
+                    curNode.left = node
+                    break
+                } else {
+                    curNode = curNode.left!!
+                }
+
+            } else if (node > curNode) {
+                if (curNode.right == null) {
+                    curNode.right = node
+                    break
+                } else {
+                    curNode = curNode.right!!
+                }
+            }
+
         }
     }
-    fun simple_delete() {}
 }
