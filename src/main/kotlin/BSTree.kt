caa7bb2aaa261414bd3d24a9d02d1b3caf98194d
Roket_Flame <danil.slinchuk@gmail.com>
2023-03-25 23:42:00 +0300

    feat: add fun get for BSTree

--- src/main/kotlin/BSTree.kt
+++ src/main/kotlin/BSTree.kt
@@ -9,6 +9,34 @@ class BSTree<K : Comparable<K>, V> : ABSTree<KeyValue<K, V>, BSNode<K, V>>() {
         return simpleContains(BSNode(data))
     }
 
+    fun get(key: K): V? {
+        if (root == null) {
+            return null
+        }
+        var curNode = root!!
+
+        while (true) {
+
+            if (key < curNode.data.getKey()) {
+                if (curNode.left == null) {
+                    break
+                } else {
+                    curNode = curNode.left!!
+                }
+
+            } else if (key > curNode.data.getKey()) {
+                if (curNode.right == null) {
+                    break
+                } else {
+                    curNode = curNode.right!!
+                }
+            } else if (key == curNode.data.getKey()) {
+                return curNode.data.getValue()
+            }
+        }
+        return null
+    }
+
     override fun delete(data: KeyValue<K, V>) {
         TODO("Not yet implemented")
     }
