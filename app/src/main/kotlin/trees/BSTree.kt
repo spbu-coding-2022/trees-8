ab537d2349fa846d7a18435224aa95f75f6e3fca
Roket_Flame <danil.slinchuk@gmail.com>
2023-03-29 07:39:43 +0300

    fix: minor changes in the use of ABSTree functions

--- app/src/main/kotlin/trees/BSTree.kt
+++ app/src/main/kotlin/trees/BSTree.kt
@@ -7,21 +7,22 @@ class BSTree<K : Comparable<K>, V> : ABSTree<KeyValue<K, V>, BSNode<K, V>>() {
 
 
     override fun add(data: KeyValue<K, V>) {
-        simpleAdd(BSNode(data))
+        root = simpleAdd(root, BSNode(data))
     }
 
     override fun contain(data: KeyValue<K, V>): Boolean {
-        return (simpleContains(BSNode(data)) != null)
+        return (simpleContains(root, BSNode(data)) != null)
     }
 
     fun get(key: K): V? {
-        return simpleContains(BSNode(KeyValue(key, null)))?.getValue()
+        return simpleContains(root, BSNode(KeyValue(key, null)))?.getValue()
     }
 
     override fun delete(data: KeyValue<K, V>) {
-        val curNode = simpleContains(BSNode(data)) ?: return
-        simpleDelete(curNode)
+        root = simpleDelete(root, BSNode(data))
     }
 
-
+    fun min(): KeyValue<K, V>? {
+        return root?.let { getMinimal(it).data }
+    }
 }
