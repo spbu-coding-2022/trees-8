164e59da366e9d50a02e289d48b43866f0e086b2
Roket_Flame <danil.slinchuk@gmail.com>
2023-04-08 15:33:49 +0300

    feat: add balancing RB tree after deletion, change process of deleting

--- app/src/main/kotlin/trees/trees/RBTree.kt
+++ app/src/main/kotlin/trees/trees/RBTree.kt
@@ -18,16 +18,109 @@ class RBTree<T : Comparable<T>> : ABSTree<T, RBNode<T>>() {
         root?.color = Color.BLACK
     }
 
+    override fun delete(data: T) {
+        if (!contain(data)) return
+
+        val node = simpleContains(root, RBNode(data)) ?: return
+        val next: RBNode<T>
+
+        if ((node.left == null) && (node.right == null)) {
+            if (node == root) {
+                root = null
+            } else {
+                if (node.color == Color.RED) {
+                    // delete red node without child
+                    replaceChild(node, null)
+                } else {
+                    // delete black node without children
+                    root = balanceDelete(node)
+                }
+            }
+            return
+        }
+
+        if ((node.left == null) || (node.right == null)) {
+            // delete for black node with one red child
+            replaceChild(node, node.right ?: node.left)?.color = Color.BLACK
+        } else {
+            // delete node with two children
+            next = getMinimal(node.right ?: error("Node must have right child"))
+            node.data = next.data.also { next.data = node.data }
+            if (next.color == Color.RED) {
+                // delete red node without child
+                replaceChild(next, null)
+            } else {
+                if (next.right == null) {
+                    root = balanceDelete(next)
+                } else {
+                    // delete for black node with one child
+                    replaceChild(next, next.right)?.color = Color.BLACK // next.right must be red and not null
+                }
+            }
+        }
+    }
+
     override fun contain(data: T): Boolean {
         return (simpleContains(root, RBNode(data)) != null)
     }
 
-    fun get(data: T): T? {
-        return simpleContains(root, RBNode(data))?.data
-    }
+    fun balanceDelete(node: RBNode<T>?): RBNode<T>? {
+        var newRoot = root
+        var current = node
 
-    override fun delete(data: T) {
-        root = simpleDelete(root, RBNode(data))
+        while ((current != newRoot) && (current?.color == Color.BLACK)) {
+            if (current == current.parent?.left) {
+                var brother = current.parent?.right
+                if (brother?.color == Color.RED) {
+                    brother.color = Color.BLACK
+                    current.parent?.color = Color.RED
+                    newRoot = clearRotateLeft(current.parent, newRoot)
+                    brother = current.parent?.right
+                }
+                if ((brother?.left?.color == Color.BLACK) && (brother.right?.color == Color.BLACK)) {
+                    brother.color = Color.RED
+                    current = current.parent
+                } else {
+                    if (brother?.right?.color == Color.BLACK) {
+                        brother.left?.color = Color.BLACK
+                        brother.color = Color.RED
+                        newRoot = clearRotateRight(brother, newRoot)
+                        brother = current.parent?.right
+                    }
+                    brother?.color = current.parent?.color ?: Color.BLACK
+                    current.parent?.color = Color.BLACK
+                    brother?.right?.color = Color.BLACK
+                    newRoot = clearRotateLeft(current.parent, newRoot)
+                    current = newRoot
+                }
+            } else {
+                var brother = current.parent?.left
+                if (brother?.color == Color.RED) {
+                    brother.color = Color.BLACK
+                    current.parent?.color = Color.RED
+                    newRoot = clearRotateRight(current.parent, newRoot)
+                    brother = current.parent?.left
+                }
+                if (brother?.right?.color == Color.BLACK && brother.left?.color == Color.BLACK) {
+                    brother.color = Color.RED
+                    current = current.parent
+                } else {
+                    if (brother?.left?.color == Color.BLACK) {
+                        brother.right?.color = Color.BLACK
+                        brother.color = Color.RED
+                        newRoot = clearRotateLeft(brother, newRoot)
+                        brother = current.parent?.left
+                    }
+                    brother?.color = current.parent?.color ?: Color.BLACK
+                    current.parent?.color = Color.BLACK
+                    brother?.left?.color = Color.BLACK
+                    newRoot = clearRotateRight(current.parent, newRoot)
+                    current = newRoot
+                }
+            }
+        }
+        current?.color = Color.BLACK
+        return newRoot
     }
 
     fun balanceAdd(initNode: RBNode<T>?, subRoot: RBNode<T>?): RBNode<T>? {
@@ -35,7 +128,6 @@ class RBTree<T : Comparable<T>> : ABSTree<T, RBNode<T>>() {
         var newRoot = subRoot
         var current = initNode
         while (current?.parent?.color == Color.RED) {
-            val grandpa = current.parent?.parent
             if (current.parent == current.parent?.parent?.left) {
                 if (current.parent?.parent?.right?.color == Color.RED) {
                     current.parent?.color = Color.BLACK
@@ -74,57 +166,8 @@ class RBTree<T : Comparable<T>> : ABSTree<T, RBNode<T>>() {
         return newRoot ?: root
     }
 
-    fun balanceDelete(initNode: RBNode<T>?): RBNode<T>? {
-        var current = initNode
-        while ((current != root) && (current?.color == Color.BLACK)) {
-            val parent = current.parent
-            val uncle = parent?.right
-            var ant = parent?.left
-            if (current == ant) {
-                if (uncle?.color == Color.RED) {
-                    uncle.color = Color.BLACK
-                    parent.color = Color.RED
-                    current.parent?.let { rotateLeft(it) }
-                }
-                if ((uncle?.right?.color == Color.BLACK) && (uncle.left?.color == Color.BLACK)) {
-                    uncle.color = Color.RED
-                } else {
-                    if (uncle?.right?.color == Color.BLACK) {
-                        uncle.left?.color = Color.BLACK
-                        uncle.color = Color.RED
-                        rotateRight(uncle)
-                    }
-                    uncle?.color = parent?.color!!
-                    current.parent?.color = Color.BLACK
-                    uncle?.right?.color = Color.BLACK
-                    current.parent?.let { rotateLeft(it) }
-                    current = root
-                }
-            } else {
-                if (ant?.color == Color.RED) {
-                    ant.color = Color.BLACK
-                    current.parent?.color = Color.RED
-                    current.parent?.let { rotateRight(it) }
-                }
-                if ((ant?.right?.color == Color.BLACK) && (ant.left?.color == Color.BLACK)) {
-                    ant.color = Color.RED
-                } else {
-                    if (ant?.left?.color == Color.BLACK) {
-                        ant.right?.color = Color.BLACK
-                        ant.color = Color.RED
-                        rotateLeft(ant)
-                    }
-                    ant = current.parent
-                    current.parent?.color = Color.BLACK
-                    ant?.left?.color = Color.BLACK
-                    current.parent?.let { rotateRight(it) }
-                    current = root
-                }
-            }
-        }
-        current?.color = Color.BLACK
-        root?.color = Color.BLACK
-        return balance(initNode)
+    fun get(data: T): T? {
+        return simpleContains(root, RBNode(data))?.data
     }
 
     fun clearRotateLeft(node: RBNode<T>?, root: RBNode<T>?): RBNode<T>? {
